f07595241a951413e25f1dceeb7d1c41dddbb366
Chaitanya07789 <chaitanyalondhe730@gmail.com>
2025-09-20 23:42:38 +0530

    product_model_updated

--- djecomm/products/models.py
+++ djecomm/products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from utils.models import BaseModel
+from accounts.models import Shopkeeper
 
 class Category(BaseModel):
     name = models.CharField(max_length=100)
@@ -31,3 +32,45 @@ class Product(BaseModel):
     hsn_code = models.CharField(max_length=100)
     maximum_retail_price = models.FloatField()
     parent_product = models.ForeignKey("Product", related_name="variant_name", on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.item_name
+    
+    def getFirstImage(self):
+        if self.product_image.first():
+            return self.product_image.first().image # reverse relation (reverse orm)
+        
+        return "https://static.vecteezy.com/system/resources/thumbnails/022/014/063/small/missing-picture-page-for-website-design-or-mobile-app-design-no-image-available-icon-vector.jpg"
+    
+
+class VarientOptions(BaseModel):
+    varient_name = models.CharField(max_length=100)
+    option_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.option_name
+
+
+class ProductVarient(BaseModel):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="product_varient")
+    varient_options = models.ManyToManyField(VarientOptions)
+
+    def __str__(self):
+        return f"Varient for {self.product.item_name}"
+
+class ProductImage(BaseModel):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="product_image")
+
+class VendorProducts(BaseModel):
+    shopkeeper = models.ForeignKey(Shopkeeper, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    vender_selling_price = models.FloatField()
+    dealer_price = models.FloatField()
+    is_active = models.BooleanField(default=True)
+    delivery_fee = models.FloatField(default=True)
+
+    def get_product_details(self):
+        return {
+            "product_name": self.product.item_name,
+            "image": self.product.getFirstImage()
+        }
